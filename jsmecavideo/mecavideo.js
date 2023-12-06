5b11b68229262b5931296bd817fd9dfbdf9e9253
VERHILLE Arnaud <gist974@gmail.com>
2023-12-06 20:26:26 +0400

    Create jsmecavideo states

--- jsmecavideo/mecavideo.js
+++ jsmecavideo/mecavideo.js
@@ -6,21 +6,23 @@ let dropdown2;
 let data = new Data();
 let graph = new Graph(data);
 
+let etat = 'Videos'; // Initialiser l'état à 'Video'
+
 // MANY THANKS TO : https://webetab.ac-bordeaux.fr/Pedagogie/Physique/site/labo/tice/c_video_tice.htm
 let videoFiles = [
-  {path: 'videos/bille1-iv5.mp4', framerate: 1000000/33333},
-  {path: 'videos/bille2-iv5.mp4', framerate: 1000000/33333},
-  {path: 'videos/bille3-iv5.mp4', framerate: 500000/16129},
-  {path: 'videos/billegrosse.mp4', framerate: 25},
-  {path: 'videos/billepetite.mp4', framerate: 25},
-  {path: 'videos/chute.mp4', framerate: 25},
-  {path: 'videos/disque33t.mp4', framerate: 25},
-  {path: 'videos/disque45t.mp4', framerate: 25},
-  {path: 'videos/moto-relatif.mp4', framerate: 25},
-  {path: 'videos/parabolique.mp4', framerate: 25},
-  {path: 'videos/vague2cm5.mp4', framerate: 20},
-  {path: 'videos/vague3.mp4', framerate: 25},
-  {path: 'videos/vague3cm0.mp4', framerate: 20}
+  { path: 'videos/bille1-iv5.mp4', framerate: 1000000 / 33333 },
+  { path: 'videos/bille2-iv5.mp4', framerate: 1000000 / 33333 },
+  { path: 'videos/bille3-iv5.mp4', framerate: 500000 / 16129 },
+  { path: 'videos/billegrosse.mp4', framerate: 25 },
+  { path: 'videos/billepetite.mp4', framerate: 25 },
+  { path: 'videos/chute.mp4', framerate: 25 },
+  { path: 'videos/disque33t.mp4', framerate: 25 },
+  { path: 'videos/disque45t.mp4', framerate: 25 },
+  { path: 'videos/moto-relatif.mp4', framerate: 25 },
+  { path: 'videos/parabolique.mp4', framerate: 25 },
+  { path: 'videos/vague2cm5.mp4', framerate: 20 },
+  { path: 'videos/vague3.mp4', framerate: 25 },
+  { path: 'videos/vague3cm0.mp4', framerate: 20 }
 ]; // Ajoutez vos fichiers vidéo ici avec leur framerate respectif obtenu grace à la commande suivante
 // # ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate vague3.mp4 
 
@@ -34,21 +36,16 @@ function setup() {
   // Créer un menu déroulant
   dropdown = createSelect();
   dropdown.parent('menu');
-
-  // Ajouter des options au menu déroulant
-  dropdown.option('Ouvrir/Enregistrer Vidéo');
-  dropdown.option('Calibrer les axes');
-  dropdown.option('Pointer les positions');
-  dropdown.option('Tracer les Graphiques');
-
-
+  dropdown.option(' Videos');
+  dropdown.option(' Calibrage');
+  dropdown.option(' Pointage');
+  dropdown.option(' Graphiques');
   // Définir une fonction de rappel pour le changement d'option
   dropdown.changed(optionChanged);
 
   // Créer un menu déroulant 2
   dropdown2 = createSelect();
   dropdown2.parent('menu');
-
   videoFiles.forEach((videoFile, index) => {
     dropdown2.option(videoFile.path, index); // Utilisez l'index comme valeur
   });
@@ -63,27 +60,52 @@ function setup() {
 }
 
 function draw() {
-  videoPlayer.draw();
+  switch (etat) {
+    case 'Videos':
+      videoPlayer.draw();
+      break;
+    case 'Calibrage':
+      videoPlayer.draw();
+      break;
+    case 'Pointage':
+      videoPlayer.draw();
+      graph.draw();
+      break;
+    case 'Graphiques':
+      background(0);
+      graph.draw();
+      break;
+    default:
+      // Code pour un état inconnu
+      console.log('Etat inconnu: ' + etat);
+  }
   visor.update(mouseX, mouseY);
   visor.draw();
-  graph.draw();
 }
 
 function optionChanged() {
-  console.log(dropdown.value());
+  etat = dropdown.value().trim(); // Mettre à jour l'état lorsque l'option change
+  console.log(etat);
+
+  if (etat !== 'Video') {
+
+  }
 }
+
 
 // Ajoute une fonction pour ajouter ou retirer un point dans data à chaque clic sur la vidéo
 function mouseClicked(event) {
-    if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-      if (mouseButton === LEFT && keyIsDown(CONTROL)) {
-        data.removePoint();
-        videoPlayer.previousFrame();
-      } else if (mouseButton === LEFT) {
-        data.addPoint(mouseX, mouseY);
-        videoPlayer.nextFrame();
-      }
+  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    if (mouseButton === LEFT && keyIsDown(CONTROL)) {
+      data.removePoint();
+      videoPlayer.previousFrame();
+    } else if (mouseButton === LEFT) {
+      data.addPoint(mouseX, mouseY);
+      videoPlayer.nextFrame();
     }
+  }
 }
+
+
 
 
